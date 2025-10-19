67de01f12090a77b8e4caca7e4c1628661d75bfb
Fady Samir <fadysamir447@gmail.com>
2025-10-19 15:22:02 +0300

    Minimal app + Dockerfile + Helm chart

---
+++ app.py
@@ -0,0 +1,13 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8080
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type","text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Tekton + Kaniko + Helm!\n")
+
+if __name__ == "__main__":
+    HTTPServer(("0.0.0.0", PORT), Handler).serve_forever()
+
